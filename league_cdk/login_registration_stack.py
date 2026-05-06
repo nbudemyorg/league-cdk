384f672b7eb27e7ac7bfd23428a290f511813455
Neil <neil.bottomley@blueyonder.co.uk>
2026-05-06 14:36:10 +0100

    Add dynamodb:DeleteItem to PasswordResetLambdaPasswordResetTableRW policy

--- league_cdk/login_registration_stack.py
+++ league_cdk/login_registration_stack.py
@@ -279,7 +279,9 @@ class LoginRegistrationStack(Stack):
 
         password_reset_table_rw = iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
-            actions=['dynamodb:GetItem', 'dynamodb:PutItem'],
+            actions=[
+                'dynamodb:GetItem', 'dynamodb:PutItem', 'dynamodb:DeleteItem'
+            ],
             resources=[password_reset_table.table_arn],
             sid='PasswordResetLambdaPasswordResetTableRW',
         )
